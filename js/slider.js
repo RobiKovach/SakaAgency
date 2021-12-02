2222e5e990f47f7058c7a780ee9df2d8f2f55429
Robert Kovach <kovachrobert1995@gmail.com>
2021-12-02 16:41:31 +0200

    new

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 40,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    }
+  },
+});
