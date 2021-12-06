b909ee0147a452eb5de666dd89137a24ebfd8a7f
Robert Kovach <kovachrobert1995@gmail.com>
2021-12-06 14:02:32 +0200

    last

--- js/slider.js
+++ js/slider.js
@@ -8,7 +8,7 @@ const swiper = new Swiper('.swiper', {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 10
+      spaceBetween: 30
     },
     // when window width is >= 480px
     480: {
